c4bfedb4405fb995ff8c06e54858ba14438d7b9a
lauraa15 <laurapuspaa15@gmail.com>
2025-05-29 17:06:43 +0700

    perlu ini

--- healy-app/startup.sh
+++ healy-app/startup.sh
@@ -1,18 +1,10 @@
-# #!/bin/bash
+#!/bin/bash
 
-# cd /home/site/wwwroot
+# Move to Laravel app root
+cd /home/site/wwwroot
 
-# # Install dependencies
-# composer install --no-dev --optimize-autoloader
+# Jalankan migration (opsional)
+php artisan migrate --force
 
-# # Set permissions
-# chmod -R 775 storage
-# chmod -R 775 bootstrap/cache
-
-# # Laravel caches
-# php artisan config:cache
-# php artisan route:cache
-# php artisan view:cache
-
-# # Jalankan server PHP internal di port 8080
-# php -S 0.0.0.0:8080 -t public
+# Jalankan Laravel
+php artisan serve --host=0.0.0.0 --port=8080
