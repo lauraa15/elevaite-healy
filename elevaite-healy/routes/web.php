ca91a4d75ac8b6f4a091044d9907383d1733a982
chintyaf <chintya.elysia@gmail.com>
2025-05-29 16:15:10 +0700

    register

--- elevaite-healy/routes/web.php
+++ elevaite-healy/routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::view('/login', 'layouts.user')->name('login');
+Route::view('/login', 'auth.login')->name('login');
 // Route::view('/login', 'login')->name('login');
 
-Route::view('/register', 'health')->name('register');
+Route::view('/register', 'auth.register')->name('register');
 
 
 
